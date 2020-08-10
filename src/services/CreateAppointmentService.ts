0440852556f2d84a05f699ce7a6ec2b88a169d99
Leonardo Pinheiro <leo.pinnheiro@gmail.com>
2020-08-10 03:21:48 -0300

    Criando classe para tratamento de erros (AppError)

--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -1,5 +1,8 @@
 import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+
 import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
@@ -25,7 +28,7 @@ class CreateAppointmentService {
 
     // Se encontrou um apontamento retorna o erro.
     if (findAppointmentInSameDate) {
-      throw Error('This appointment is already booked.');
+      throw new AppError('This appointment is already booked.');
     }
 
     // Cria o agendamento
