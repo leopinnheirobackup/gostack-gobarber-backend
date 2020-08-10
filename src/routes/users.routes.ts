7ca801f24d52ce850a0b9dbbb8f1515490179aae
Leonardo Pinheiro <leo.pinnheiro@gmail.com>
2020-08-10 02:52:23 -0300

    Criacao de rota para atualizacao de foto de avatar com multer.

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,12 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
+
 import CreateUserService from '../services/CreateUserService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRoutes = Router();
+const upload = multer(uploadConfig);
 
 usersRoutes.post('/', async (request, response) => {
   try {
@@ -17,5 +22,14 @@ usersRoutes.post('/', async (request, response) => {
     return response.status(400).json({ error: err.message });
   }
 });
+
+usersRoutes.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    return response.json({ ok: true });
+  },
+);
 
 export default usersRoutes;
